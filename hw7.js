487c50a01bfc96a5f2027a9629dcb0d3d26c82db
ayazimahsa <mahsaayazii73@gmail.com>
2022-05-19 03:09:39 +0430

    add hw7

---
+++ hw7.js
@@ -0,0 +1,17 @@
+let animal = {
+  speaks: true,
+  walk() {
+    console.log("Animal walks");
+  },
+}
+
+// ..........................................
+
+let dog = {
+    name: "White dog",
+  __proto__: animal
+};
+
+console.log(dog.speaks);
+
+
